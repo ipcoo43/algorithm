043bd914132a3b28a414351bb267143705ca58c8
ipcoo43 <ipcoo43@gmail.com>
2019-04-29 22:19:03 +0000

    2019-04-29 lesson

---
+++ lesson126.py
@@ -0,0 +1,15 @@
+print('행고정 열변화')
+for x in range(1,6):   # 행
+	for y in range(1,6): # 열
+		print('({},{})'.format(x,y), end=' ')
+	print()
+print()
+
+print('열고정 행변화')
+for x in range(1,6):   # 행
+	for y in range(1,6): # 열
+		print('({},{})'.format(y,x), end=' ')
+	print()
+print()
+
+
